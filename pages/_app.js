84b15837078c52ccce3bde0eaa88276084d75efd
elducati <geomondi09@gmail.com>
2022-06-20 03:36:46 +0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider sdkOptions={{
+      gasless: {
+        openzeppelin: {
+          relayerUrl: process.env.NEXT_PUBLIC_OPENZEPPELIN_URL,
+        },
+      },
+    }} desiredChainId={ChainId.Mumbai}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useMetamask, useAddress, useEditionDrop } from "@thirdweb-dev/react";
 
 export default function Home() {
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const editionDrop = useEditionDrop("0x270eDec8324F54Dda71224BDc8E8492206d4232b");
+  const tokenId = 0;
+  const quantity = 1;
+
+  const claimNFT = async () => {
+    try {
+      await editionDrop?.claimTo(address, tokenId, quantity);
+      console.log("🎉 NFT claimed successfully!");
+    } catch (err) {
+      console.log("💩 Error claiming NFT: ", err);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,44 +27,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.container}>
+          {address ? (
+            <>
+            <h2>You are signed in as {address}</h2>
+            <button onClick={claimNFT}>Claim NFT</button>
+            </>
+          ) : (
+            <button onClick={connectWithMetamask}>Sign in with metamask</button>
+          )}
         </div>
       </main>
 
@@ -62,6 +51,7 @@ export default function Home() {
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
+          <span> |& El Ducati</span>
         </a>
       </footer>
     </div>
